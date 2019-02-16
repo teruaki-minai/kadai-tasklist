66e249af169936182e76383705e9a0c76a5d539f
t.minai <seisi0wakwbl.ue@gmail.com>
2019-02-16 09:49:49 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@
 
 Route::get('/', "TasksController@index");
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::resource("tasks", "TasksController");
