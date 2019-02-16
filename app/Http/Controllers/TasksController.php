dc1421f996065b37103d385a35e91e7f1448d79e
t.minai <seisi0wakwbl.ue@gmail.com>
2019-02-16 11:53:33 +0000

    minor change

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,7 +15,7 @@ class TasksController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::orderBy('id', 'desc');
         return view("tasks.index", [
             "tasks" => $tasks, 
             ]);
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,8 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('tasks', 'TasksController', ['only' => ['index', 'show',"create","edit"]]);
+});
 
 Route::resource("tasks", "TasksController");
